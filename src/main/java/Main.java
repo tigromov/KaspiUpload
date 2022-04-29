20e21748756ca0284248e2c5d65034a4d3843130
tigromov <102591826+tigromov@users.noreply.github.com>
2022-04-29 16:28:18 +0600

    Yep! + 10proc roznica greg bennet discount

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -10,15 +10,17 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.sql.SQLOutput;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Objects;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         ///////сссылки//////
         String kaspiUrl = "C:/Rest/Test/KaspiUPL.xlsx";
-        String restUrl = "C:/Rest/Test/130422.xls";
-        String midUrl = "C:/Rest/Test/ParsingMid2.xls";
+        String restUrl = "C:/Rest/Test/290422.xls";
+        String midUrl = "C:/Rest/Test/2604mid.xls";
 
         ///////подключение к файлу остатков////////////
         FileInputStream fis = new FileInputStream(restUrl);
@@ -49,6 +51,34 @@ public class Main {
 
 
         }
+
+        /////////////////////////////////////////////////////Защита от резерва/////////
+        ArrayList restReservedList = new ArrayList();
+        for (int i = 0; i < restRows; i++) {
+            try{switch (restSheet.getRow(i).getCell(2).getCellType()){
+                case STRING -> {
+                    restReservedList.add(restSheet.getRow(i).getCell(2).getStringCellValue());
+                    break;
+                }
+                case NUMERIC -> {
+                    restReservedList.add(Objects.toString((int)restSheet.getRow(i).getCell(2).getNumericCellValue()));
+                    break;
+                }
+                case BLANK, ERROR,_NONE, BOOLEAN, FORMULA -> {
+                    restReservedList.add(Objects.toString(restSheet.getRow(i).getCell(2).getStringCellValue()));
+                    break;
+                }
+            }}catch (NullPointerException e){
+                restReservedList.add(" ");
+
+            }
+
+
+        }
+
+        int rsrSave = restRows-restReservedList.indexOf("РЕЗЕРВ");
+        System.out.println("Количество наименований в резерве: " + rsrSave);
+        ///////////////////////////////////////////////////////////////////////////////
 
         if(restList.size()==restRows){
             System.out.println("Файл остатков считан верно, всего строк в файле остатков: " + restRows + " Считалось: " + restList.size());
@@ -102,7 +132,15 @@ public class Main {
         int trdPrice;
         int nxtPrice;
         int lowestPrice;
+        int specialPrice;
         String skuMid;
+        String skuRest;
+        String yes = "yes";
+        String no = "no";
+        String name;
+        String brand;
+
+
         for (int i = 1; i <= midRows; i++) {
             ////////////если на первом месте MusicPark и больше нет продавцов - то цена Розница/////////////
             if(Objects.equals(midSheet.getRow(i).getCell(3).getStringCellValue(), "MusicPark")){
@@ -132,6 +170,8 @@ public class Main {
 
 
                 }
+                  ////////////////если конкурент поставил цену ниже, то ставим еще ниже на 2%, если она не принижает закуп
+                //////////////////+13%, если принижает, то ставим розницу
 
 
             }else{try{
@@ -139,7 +179,7 @@ public class Main {
                 skuMid = midSheet.getRow(i).getCell(0).getStringCellValue();
                 inPrice = (int) restSheet.getRow(restList.indexOf(skuMid)).getCell(4).getNumericCellValue();
                 trdPrice = (int)restSheet.getRow(restList.indexOf(skuMid)).getCell(6).getNumericCellValue();
-                trdPrice = (int)(trdPrice*1.15);
+
                 if(lowestPrice > (inPrice*1.13)){
                     if(lowestPrice>trdPrice){actualPrice = trdPrice;midSheet.getRow(i).createCell(5).setCellValue(actualPrice);}
                     actualPrice=(int)(lowestPrice*0.98);
@@ -160,9 +200,79 @@ public class Main {
         fos.close();
 
 
+///////////////////формирование каспи////////////
+        FileInputStream fisKaspi = new FileInputStream(kaspiUrl);
+        XSSFWorkbook kspbook = new XSSFWorkbook(fisKaspi);
+        XSSFSheet kaspiSheet = (XSSFSheet) kspbook.getSheetAt(0);
+
+///////////////////////////////////////////////////////////
+        ArrayList midSkuList = new ArrayList();
+        for (int i = 1; i < midRows+1; i++) {
+            midSkuList.add(midSheet.getRow(i).getCell(0).getStringCellValue());
+
+
+        }
 
 
 
+
+        for (int i = 1; i < restList.size()-rsrSave; i++) {
+            if (restSkuList.contains(restList.get(i))){
+                /////артикул///////
+                int kaspiLastRaw = kaspiSheet.getLastRowNum()+1;
+                skuRest = Objects.toString(restList.get(i));
+                kaspiSheet.createRow(kaspiLastRaw).createCell(0).setCellValue(skuRest);
+
+                /////название//////
+
+                name = restSheet.getRow(i).getCell(2).getStringCellValue();
+                kaspiSheet.getRow(kaspiLastRaw).createCell(1).setCellValue(String.valueOf(name));
+
+                /////бренд////////
+                int index = name.indexOf(' ');
+                brand = name.substring(0, index);
+                kaspiSheet.getRow(kaspiLastRaw).createCell(2).setCellValue(String.valueOf(brand));
+
+                /////////////ПОПРАВКИ//////////////
+
+
+                
+                /////////цена//////////
+                try{if (brand.equals("GregBennett")){
+                    specialPrice = (int)restSheet.getRow(i).getCell(5).getNumericCellValue();
+                    kaspiSheet.getRow(kaspiLastRaw).createCell(3).setCellValue(specialPrice);
+                    System.out.println("GregBennet дилер прайс " + specialPrice + name);
+                }else{
+                actualPrice = (int)midSheet.getRow(midSkuList.indexOf(skuRest)+1).getCell(5).getNumericCellValue();
+                kaspiSheet.getRow(kaspiLastRaw).createCell(3).setCellValue(actualPrice);}}
+                catch (NullPointerException e){
+                    if (brand.equals("GregBennett")){
+                        specialPrice = (int)restSheet.getRow(i).getCell(5).getNumericCellValue();
+                        kaspiSheet.getRow(kaspiLastRaw).createCell(3).setCellValue(specialPrice);
+                        System.out.println("GregBennet дилер прайс " + specialPrice + name);
+                    }else{
+
+                    actualPrice = (int)restSheet.getRow(restList.indexOf(skuRest)).getCell(6).getNumericCellValue();
+                    kaspiSheet.getRow(kaspiLastRaw).createCell(3).setCellValue(actualPrice);
+                }}
+
+
+                ///////////yes///////////
+                kaspiSheet.getRow(kaspiLastRaw).createCell(4).setCellValue(yes);
+                kaspiSheet.getRow(kaspiLastRaw).createCell(5).setCellValue(no);
+                kaspiSheet.getRow(kaspiLastRaw).createCell(6).setCellValue(no);
+                kaspiSheet.getRow(kaspiLastRaw).createCell(7).setCellValue(no);
+                kaspiSheet.getRow(kaspiLastRaw).createCell(8).setCellValue(no);
+
+            }
+
+
+
+        }
+
+        FileOutputStream fosksp = new FileOutputStream(kaspiUrl);
+        kspbook.write(fosksp);
+        fosksp.close();
 
 
 
